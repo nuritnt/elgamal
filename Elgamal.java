1f2227b98a6445ca4551aaa3a41b58c01c40bbeb
Tuğçe Nur Taş <tugcenur@required.ch>
2023-05-19 22:42:12 +0200

    Rename Main file and delete old code

---
+++ Elgamal.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.SecureRandom;
+import java.util.List;
+
+public class Elgamal {
+
+    public static void main(String[] args) {
+    }
+
+    public static void generateKeys() {
+    }
+
+}
